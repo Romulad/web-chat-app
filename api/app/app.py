e1133399d43be6da6b672d17f5bda05cc42016f8
Romuald Oluwatobi <romualdnoualinon@gmail.com>
2024-10-15 20:15:36 +0100

    update CORS

--- api/app/app.py
+++ api/app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .database import get_db_lifespan
 from .routers.auth import router as auth_router
@@ -10,3 +11,15 @@ app = FastAPI(lifespan=get_db_lifespan())
 app.include_router(auth_router)
 app.include_router(chat_router)
 app.include_router(open_chat_router)
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
