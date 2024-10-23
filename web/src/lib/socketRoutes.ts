9996d2fd43ba16fe070596b7a0836086019ab602
Romuald Oluwatobi <romualdnoualinon@gmail.com>
2024-10-23 20:49:08 +0100

    Update socket url for test deployment

--- web/src/lib/socketRoutes.ts
+++ web/src/lib/socketRoutes.ts
@@ -1,6 +1,6 @@
-import { BASE_ROUTE } from "../api/constant"
+// import { BASE_ROUTE } from "../api/constant"
 
 
 export const getOpenChatSocketRoute = ( ) => {
-    return BASE_ROUTE + "open-chat/ws"
+    return location.href.includes('localhost') ? "ws://127.0.0.1:8000/open-chat/ws" : "wss://web-chat-app-0r03.onrender.com/open-chat/ws";
 }
