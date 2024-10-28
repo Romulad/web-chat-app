4efe5fc5afb5249c555f0b61958a01f33b07412a
Romuald Oluwatobi <romualdnoualinon@gmail.com>
2024-10-28 17:29:09 +0100

    Before checking last commit

--- api/app/app.py
+++ api/app/app.py
@@ -4,8 +4,18 @@ from fastapi.middleware.cors import CORSMiddleware
 from .routers.open_chat import router as open_chat_router
 
 
-app = FastAPI()
+app = FastAPI(
+    description="""Backend for a chat application that let `people chat in a open way`.\n
+    1- Create a chat
+    2- Share link to invite people to join
+    3- Confirm join requests
+    4- Chat
+    5- Once done click delete, to delete everything""", 
+    title="Open chat",
+    summary="Server for a chat application" 
+)
 app.include_router(open_chat_router)
+
 
 origins = [
     "http://localhost:5173",
