5fcd1c46eda611c1dd213290e8f703c49793a39c
Romuald Oluwatobi <romualdnoualinon@gmail.com>
2024-12-02 16:43:03 +0100

    Playing with git hooks

--- web/src/pages/openChatPage/index.tsx
+++ web/src/pages/openChatPage/index.tsx
@@ -9,7 +9,7 @@ import classes from "../../lib/classes";
 export default function OpenChatPage(){
     const chatData = getUseropenChatData();
     const [chatId, setChatId] = useState<string>("");
-    const [showDeleteChatModal, setDeleteChatModal] = useState(false);
+    const [showDeleteChatModal, setDeleteChatModal] = useState<boolean>(false);
 
     function toggleDeleteChatModal(){
         setDeleteChatModal(!showDeleteChatModal)
@@ -19,31 +19,31 @@ export default function OpenChatPage(){
         <>
         <div className="h-screen overflow-hidden landing-page-bg bg-slate-100 flex items-center">
             <div className="w-full sm:w-[500px] sm:mx-auto mx-3 max-h-[500px] overflow-auto mt-10 bg-white shadow-lg rounded-lg p-4">
-                {chatData && chatData.length ?(
+                {chatData && chatData.length ? (
                     <ul className="divide-y-2">
-                {chatData?.map((data)=>(
-                    <li key={data.chatId} className="py-3 flex items-center gap-2">
-                        <Link to={getOpenChatPath(data.chatId)}
-                        className="grow w-full flex flex-col gap-1 p-3 rounded-lg hover:bg-slate-100 ">
-                            <span>
-                                {data.chatName}
-                            </span>
-                            <span className="text-gray-600 text-sm">
-                                {data.chatId.slice(0, 30) + "..."}
-                            </span>
-                        </Link>
+                        {chatData?.map((data)=>(
+                            <li key={data.chatId} className="py-3 flex items-center gap-2">
+                                <Link to={getOpenChatPath(data.chatId)}
+                                className="grow w-full flex flex-col gap-1 p-3 rounded-lg hover:bg-slate-100 ">
+                                    <span>
+                                        {data.chatName}
+                                    </span>
+                                    <span className="text-gray-600 text-sm">
+                                        {data.chatId.slice(0, 30) + "..."}
+                                    </span>
+                                </Link>
 
-                        <button
-                        onClick={()=>{
-                            toggleDeleteChatModal();
-                            setChatId(data.chatId);
-                        }}>
-                            <TrashIcon 
-                            className="size-6 text-red-500"
-                            />
-                        </button>
-                    </li>
-                ))}
+                                <button
+                                onClick={()=>{
+                                    toggleDeleteChatModal();
+                                    setChatId(data.chatId);
+                                }}>
+                                    <TrashIcon 
+                                    className="size-6 text-red-500"
+                                    />
+                                </button>
+                            </li>
+                        ))}
                     </ul>
                 ) : (
                     <div className={"flex justify-center"}>
